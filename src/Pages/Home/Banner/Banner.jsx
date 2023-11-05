0c8178966085c941fe21da25ff1a04ffea594986
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-11-05 11:06:57 +0600

    style- navbar,index.html title and fav icon

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,19 +5,19 @@ import "aos/dist/aos.css";
 const Banner = () => {
   AOS.init();
   return (
-    <div className="flex min-h-screen justify-center items-center bg-custom-light1 px-6">
-      <div data-aos="fade-left" className="w-1/2">
-        <h1 className="text-7xl font-bold text-custom-main2">
+    <div className="flex flex-col lg:flex-row min-h-screen justify-center items-center bg-custom-light1 px-6">
+      <div data-aos="fade-left" className="lg:w-1/2 text-center lg:text-left">
+        <h1 className="text-2xl lg:text-7xl font-bold text-custom-main2">
           Where Pages Come to Life
           <span className="text-custom-main ml-2">
             A Library Beyond Boundaries.
           </span>
         </h1>
-        <button className="btn bg-custom-main text-white font-semibold mt-6">
+        <button className="btn bg-custom-main text-white font-semibold mt-6 hover:bg-white hover:text-custom-main hover:border-2 hover:border-custom-main">
           Browse Collection
         </button>
       </div>
-      <div data-aos="fade-right" className="w-1/2">
+      <div data-aos="fade-right" className="lg:w-1/2">
         <img className="w-full" src={banner} alt="banner-pic" />
       </div>
     </div>
